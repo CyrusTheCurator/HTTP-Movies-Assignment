ba3446118f67a444fbb2069f1d08f9848df4d139
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-04-28 17:16:25 -0700

    Added put, delete, and post functionality. Also incorporated basic form length validation, some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import AddMovie from "./Movies/AddMovie";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
 
-const App = () => {
+const App = (props) => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
@@ -29,12 +30,47 @@ const App = () => {
       <SavedList list={savedList} />
 
       <Route exact path="/">
+        <div className="addMovieContainer">
+          {" "}
+          <h2>Add your own movie -</h2>{" "}
+          <Link to="/add-movie">Add New Movie</Link>
+        </div>
         <MovieList movies={movieList} />
       </Route>
-
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+      <Route path="/add-movie">
+        <AddMovie
+          {...props}
+          setMovieList={setMovieList}
+          movieList={movieList}
+        />
       </Route>
+<<<<<<< Updated upstream
+=======
+      <Route
+        path="/movies/:id"
+        render={(props) => {
+          return (
+            <Movie
+              {...props}
+              addToSavedList={addToSavedList}
+              setMovieList={setMovieList}
+              movieList={movieList}
+            />
+          );
+        }}
+      ></Route>
+
+      <Route
+        path="/update-movie/:id"
+        render={(props) => (
+          <MovieUpdate
+            {...props}
+            setMovieList={setMovieList}
+            movieList={movieList}
+          />
+        )}
+      />
+>>>>>>> Stashed changes
     </>
   );
 };
